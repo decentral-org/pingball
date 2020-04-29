aa8bc811d8640bd9eea9ea4f5bdc961b3a8946c0
Fabien Contoli <fabien.contoli@loreal.com>
2020-04-29 13:26:54 +0200

    [evo] test heroku

--- sources/client.js
+++ sources/client.js
@@ -3,6 +3,8 @@ import * as scenes from 'scenes/index.js';
 
 import {Theatre} from 'core/theatre.js';
 
+window.serverUrl="https://infinite-brushlands-09118.herokuapp.com/";
+
 new Theatre({
     'assets': assets,
     'container': document.body,
