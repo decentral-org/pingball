1b7c7f765b9e3dee6b12d6cc6e90ef255e23395f
Tony <lolilol@lol.com>
2020-11-12 17:13:31 +0100

    Adding proxy, still no websocket config

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 
 # Start as client
 if [ "$START_AS_CLIENT" == "true" ]; then
+    chmod +x app_conf_envsubst.sh
+    ./app_conf_envsubst.sh
+    service nginx start
     npm run client:prod
 else # or by default: start what is defined in CMD
     exec "$@"
--- sources/client.js
+++ sources/client.js
@@ -4,7 +4,7 @@ import * as scenes from 'scenes/index.js';
 import {Theatre} from 'core/theatre.js';
 
 //window.serverUrl="https://pingpong-experiment.herokuapp.com/";
-window.serverUrl="http://localhost:3000/";
+window.serverUrl = "http://localhost:3001";
 
 new Theatre({
     'assets': assets,
